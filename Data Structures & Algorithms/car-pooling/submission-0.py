5d1a5b64a58630485e65d54ae2ce1679c5e84c29
Abhijit Kiran Valluri <abhijitvalluri@users.noreply.github.com>
2026-04-19 15:35:21 +0100

    Add: car-pooling - submission-0

---
+++ Data Structures & Algorithms/car-pooling/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        load = 0
+        minHeap = []
+
+        for trip in trips:
+            numPass, src, dst = trip
+            # We are currently at src location. All passengers who get off at or before src need to be removed from load
+            while minHeap and minHeap[0][0] <= src:
+                load -= heapq.heappop(minHeap)[1]
+            
+            if numPass + load > capacity:
+                return False
+            else:
+                load += numPass
+                heapq.heappush(minHeap, (dst, numPass))
+        
+        return True
